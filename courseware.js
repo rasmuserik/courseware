3276b237dee72d58824ffc8c860da41111c63758
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-05-31 14:56:27 +0200

    use create-react-app instead

--- courseware.js
+++
@@ -1,6 +0,0 @@
-// # courseware
-//
-// **Under development, not ready yet**
-//
-// The purpose of this project,
-// is to make it easy to make online courses.
--- lib.js
+++
@@ -1,3 +0,0 @@
-// # courseware
-//
-console.log('hello');
